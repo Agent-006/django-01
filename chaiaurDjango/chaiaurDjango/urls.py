2dc5888dde019781e9c7b84a4a5497cc9ea6919c
Sagar~ <sagargope2001@gmail.com>
2024-05-30 02:27:25 +0530

    added database and models

--- chaiaurDjango/chaiaurDjango/urls.py
+++ chaiaurDjango/chaiaurDjango/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -29,4 +31,4 @@ urlpatterns = [
     
     # always keep this at last, bcoz it send a loads of things, not used in production
     path("__reload__/", include("django_browser_reload.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
